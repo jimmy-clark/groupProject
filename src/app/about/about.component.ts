14b27ba1dfb73ef8bf68d977eb3fa0cc7b4d0839
James Clark <jamesclark@swuser-129-8-226-24.swlan.csufresno.edu>
2019-12-17 12:09:10 -0800

    added flex modal

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { FlexModalService } from '../shared-components/flex-modal/flex-modal.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  errorMessage = '';
   constructor(
     private router: Router,
-    private routerModule: RouterModule
+    private routerModule: RouterModule,
+    private flexModal: FlexModalService
   ) { }
 
   ngOnInit() {
   }
-
+  showModal() {
+    this.errorMessage = 'Testing';
+    this.flexModal.openDialog('error-modal', null);
+  }
 }
